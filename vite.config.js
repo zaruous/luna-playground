ef1f694f65b64f04ef2f628891f5615ecab8a09b
KimSudal <115706921+zaruous@users.noreply.github.com>
2026-08-24 10:45:08 +0900

    Set default dev server port to 15173

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './',
-  server: { host: '127.0.0.1', port: 5173, strictPort: true },
+  server: { host: '127.0.0.1', port: 15173, strictPort: true },
 });
